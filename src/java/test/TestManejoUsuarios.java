4e217360ce293e932f586f213859d414a86d1baf
Jhumberto <jhumbertto@gmail.com>
2023-06-29 00:03:14 -0600

    test

--- src/java/test/TestManejoUsuarios.java
+++ src/java/test/TestManejoUsuarios.java
@@ -1,4 +1,3 @@
-
 package test;
 
 /**
@@ -10,10 +9,16 @@ import domain.Usuario;
 import java.util.List;
 
 public class TestManejoUsuarios {
+
     public static void main(String[] args) {
         UsuarioDAO usuarioDao = new UsuarioDAO();
+        // Insertando un nuevo objeto de tipo Usuario
+
+        Usuario usuarioNuevo = new Usuario("Migue", "123456");
+        usuarioDao.insertar(usuarioNuevo);
+
         List<Usuario> usuarios = usuarioDao.seleccionar();
-        for(Usuario usuario: usuarios){
+        for (Usuario usuario : usuarios) {
             System.out.println("Usuario = " + usuario);
         }
     }
